ed4668f7f861c4a014f0ed6fc1812bae834d968b
selenne moreno <karenselennemoreno@gmauil.com>
2025-12-29 18:34:43 -0500

    Fix conexión Google Sheets y lógica app

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ creds = Credentials.from_service_account_info(
 
 client = gspread.authorize(creds)
 
-archivo = client.open("registro_horas_brian")
+archivo = client.open_by_key("1MDS9f1cLCe99VgNcqJzOZwp3Rbql5H7LTjqigJZWb9o")
+
 sheet_eventos = archivo.sheet1
 sheet_semanas = archivo.worksheet("semanas")
 
